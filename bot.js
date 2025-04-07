a5faac1c187187fc941487e2f7d7a942fdc94745
RizzMDOFC <156505702+RizzMDOFC@users.noreply.github.com>
2025-04-07 21:30:43 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,12 +23,12 @@ client.initialize();
 function sendOtp(phone, otp) {
   const cleaned = phone.replace(/\D/g, '');
   const normalized = cleaned.startsWith('0') ? '62' + cleaned.slice(1) : cleaned;
-  const chatId = `${normalized}@c.us`;
+  const chatId = ${normalized}@c.us;
 
   console.log('➡️ Mengirim OTP ke', chatId);
 
-  client.sendMessage(chatId, `Kode OTP kamu untuk login RizzShop adalah: *${otp}*`)
-    .then(() => console.log(`✅ OTP terkirim ke ${chatId}`))
+  client.sendMessage(chatId, `🔐 OTP untuk login RizzShop:\n\n*${otp}*\n\n > 📌 Kode ini berlaku 5 menit.\n\n_Salin manual kode di atas ke aplikasi._`)
+    .then(() => console.log(✅ OTP terkirim ke ${chatId}))
     .catch(err => console.error('❌ Gagal kirim OTP:', err.message));
 }
 
