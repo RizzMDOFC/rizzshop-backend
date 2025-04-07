30018099b3c9f7154edb1686bb7832f2c26dbe69
RizzMDOFC <156505702+RizzMDOFC@users.noreply.github.com>
2025-04-07 23:06:29 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-  authStrategy: new LocalAuth(),
+  session: sessionData,
   puppeteer: {
     headless: true,
-    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   }
 });
 
